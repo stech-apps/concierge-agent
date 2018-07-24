13cbd366f4fb000045dc5e0c375823c9e9990c77
Dimuthu Perera <dimuthu.p@creativesoftware.com>
2018-07-24 16:17:59 +0530

    159232005: Implement theme support in concierge: Ie support

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
+import cssVars from 'css-vars-ponyfill';
 
 @Injectable()
 export class Util {
     constructor() {
     }
 
-    compareVersions(baseVersion, currentVersion){
+    compareVersions(baseVersion, currentVersion) {
         if (typeof baseVersion + typeof currentVersion != '')
             return -1;
-    
+
         var a = baseVersion.split('.')
-        ,   b = currentVersion.split('.')
-        ,   i = 0, len = Math.max(a.length, b.length);
-        
+            , b = currentVersion.split('.')
+            , i = 0, len = Math.max(a.length, b.length);
+
         for (; i < len; i++) {
             if ((a[i] && !b[i] && parseInt(a[i]) > 0) || (parseInt(a[i]) > parseInt(b[i]))) {
                 return 1;
@@ -30,10 +31,22 @@ export class Util {
                 themeColor = servicePoint.parameters.customizeHighlightColor;
             }
 
+
             //set color for app theme custom property
             if (themeColor) {
                 let styles: any = getComputedStyle(document.documentElement);
                 document.documentElement.style.setProperty('--app-theme', themeColor);
+
+                //IE fix 
+                if (!(window["CSS"] && CSS.supports('color', 'var(--primary)'))) {
+                    setTimeout(() => {
+                        cssVars({
+                            variables: {
+                                'app-theme': themeColor
+                            }
+                        });
+                    }, 0);
+                }
             }
         }
     }

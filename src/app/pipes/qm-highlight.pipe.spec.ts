1a8463a41093188f752cd232fa5b2949fcea5460
Dimuthu Perera <dimuthu.p@creativesoftware.com>
2018-10-16 16:22:35 +0530

    branch changes

---
+++ src/app/pipes/qm-highlight.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { QmHighlightPipe } from './qm-highlight.pipe';
+
+describe('QmHighlightPipe', () => {
+  it('create an instance', () => {
+    const pipe = new QmHighlightPipe();
+    expect(pipe).toBeTruthy();
+  });
+});

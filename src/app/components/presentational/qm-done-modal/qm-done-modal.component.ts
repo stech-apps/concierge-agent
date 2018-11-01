3eb9c69a6101babcf921bc48f0d026c56c920fc8
Dimuthu Perera <dimuthu.p@creativesoftware.com>
2018-11-01 11:13:24 +0530

    icomoon updated fixed confirm options icons

--- src/app/components/presentational/qm-done-modal/qm-done-modal.component.ts
+++ src/app/components/presentational/qm-done-modal/qm-done-modal.component.ts
@@ -1,24 +1,36 @@
-import { Observable } from 'rxjs';
+import { Observable, Subject, Subscription } from 'rxjs';
 import { UserSelectors } from 'src/store';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'qm-done-modal',
   templateUrl: './qm-done-modal.component.html',
   styleUrls: ['./qm-done-modal.component.scss']
 })
-export class QmDoneModalComponent implements OnInit {
+export class QmDoneModalComponent implements OnInit, OnDestroy {
 
   heading: string = '';
   subHeading: string = '';
   fieldList: Array<{icon: string, label: string}> = [];
 
-  userDirection$: Observable<string>
+  userDirection$: Observable<string>;
+  userDirection: string;
+  subscriptions: Subscription  = new Subscription();
 
   constructor(public activeModal: NgbActiveModal, private userSelectors: UserSelectors) { }
 
   ngOnInit() {
     this.userDirection$ = this.userSelectors.userDirection$;
+
+    const userSubscription = this.userDirection$.subscribe((ud)=> {
+      this.userDirection = ud;
+    });
+
+    this.subscriptions.add(userSubscription);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 }

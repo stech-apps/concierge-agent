8b226bde0aea59a7a81ccb49248fa2efae09463c
dimuthu <idimuthu@gmail.com>
2018-11-13 10:01:51 +0530

    160978161: Edit appointment - Reschedule

--- src/app/components/presentational/qm-reschedule/qm-reschedule.component.ts
+++ src/app/components/presentational/qm-reschedule/qm-reschedule.component.ts
@@ -1,21 +1,39 @@
-import { QmModalService } from './../qm-modal/qm-modal.service';
-import { IAppointment } from './../../../../models/IAppointment';
-import { ICalendarService } from './../../../../models/ICalendarService';
-import { IBranch } from './../../../../models/IBranch';
+import { QmModalService } from "./../qm-modal/qm-modal.service";
+import { IAppointment } from "./../../../../models/IAppointment";
+import { ICalendarService } from "./../../../../models/ICalendarService";
+import { IBranch } from "./../../../../models/IBranch";
 import {
-  CalendarBranchSelectors, UserSelectors, BranchSelectors, ReserveDispatchers,
-  ReserveSelectors, TimeslotDispatchers, ReservationExpiryTimerDispatchers,
-  AppointmentDispatchers, AppointmentSelectors, InfoMsgDispatchers
-} from './../../../../store';
-import { ICalendarBranch } from './../../../../models/ICalendarBranch';
-import { Subscription, Observable } from 'rxjs';
-import { Component, OnInit, OnDestroy, Input, SimpleChanges, EventEmitter, Output } from '@angular/core';
-import { CalendarDate } from 'src/app/components/containers/qm-calendar/qm-calendar.component';
-import * as moment from 'moment';
-import { IBookingInformation } from 'src/models/IBookingInformation';
-import { CalendarServiceSelectors, ServicePointSelectors } from 'src/store/services';
-import { TranslateService } from '@ngx-translate/core';
-
+  CalendarBranchSelectors,
+  UserSelectors,
+  BranchSelectors,
+  ReserveDispatchers,
+  ReserveSelectors,
+  TimeslotDispatchers,
+  ReservationExpiryTimerDispatchers,
+  AppointmentDispatchers,
+  AppointmentSelectors,
+  InfoMsgDispatchers
+} from "./../../../../store";
+import { ICalendarBranch } from "./../../../../models/ICalendarBranch";
+import { Subscription, Observable } from "rxjs";
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  Input,
+  SimpleChanges,
+  EventEmitter,
+  Output
+} from "@angular/core";
+import { CalendarDate } from "src/app/components/containers/qm-calendar/qm-calendar.component";
+import * as moment from "moment";
+import { IBookingInformation } from "src/models/IBookingInformation";
+import {
+  CalendarServiceSelectors,
+  ServicePointSelectors,
+  SystemInfoSelectors
+} from "src/store/services";
+import { TranslateService } from "@ngx-translate/core";
 
 enum RescheduleState {
   Default = 1,
@@ -23,9 +41,9 @@ enum RescheduleState {
 }
 
 @Component({
-  selector: 'qm-reschedule',
-  templateUrl: './qm-reschedule.component.html',
-  styleUrls: ['./qm-reschedule.component.scss']
+  selector: "qm-reschedule",
+  templateUrl: "./qm-reschedule.component.html",
+  styleUrls: ["./qm-reschedule.component.scss"]
 })
 export class QmRescheduleComponent implements OnInit, OnDestroy {
   currentlyActiveDate: CalendarDate;
@@ -44,9 +62,10 @@ export class QmRescheduleComponent implements OnInit, OnDestroy {
   isRescheduleEnabledInUtt: boolean = true;
   isDeleteEnabledInUtt: boolean = true;
   isOriginalAppointmentTimeChanged = false;
+  timeConvention: string = "24";
 
   currentRescheduleState: RescheduleState = RescheduleState.Default;
-  selectedDates: CalendarDate[]
+  selectedDates: CalendarDate[];
 
   @Input()
   editAppointment: IAppointment;
@@ -54,81 +73,107 @@ export class QmRescheduleComponent implements OnInit, OnDestroy {
   @Output()
   onFlowExit: EventEmitter<any> = new EventEmitter();
 
-  constructor(private userSelectors: UserSelectors, private branchSelectors: BranchSelectors,
-    private reserveSelectors: ReserveSelectors, private reserveDispatchers: ReserveDispatchers,
-    private calendarServiceSelectors: CalendarServiceSelectors, private timeSlotDispatchers: TimeslotDispatchers,
-    private qmModalService: QmModalService, private reservationExpiryTimerDispatchers: ReservationExpiryTimerDispatchers,
-    private appointmentDispatchers: AppointmentDispatchers, private appointmentSelectors: AppointmentSelectors,
-    private calendarBranchSelectors: CalendarBranchSelectors, private infoMessageDispatchers: InfoMsgDispatchers,
-    private translationService: TranslateService, private servicePointSelectors: ServicePointSelectors) {
-
+  constructor(
+    private userSelectors: UserSelectors,
+    private branchSelectors: BranchSelectors,
+    private reserveSelectors: ReserveSelectors,
+    private reserveDispatchers: ReserveDispatchers,
+    private calendarServiceSelectors: CalendarServiceSelectors,
+    private timeSlotDispatchers: TimeslotDispatchers,
+    private qmModalService: QmModalService,
+    private reservationExpiryTimerDispatchers: ReservationExpiryTimerDispatchers,
+    private appointmentDispatchers: AppointmentDispatchers,
+    private appointmentSelectors: AppointmentSelectors,
+    private calendarBranchSelectors: CalendarBranchSelectors,
+    private infoMessageDispatchers: InfoMsgDispatchers,
+    private translationService: TranslateService,
+    private servicePointSelectors: ServicePointSelectors,
+    private systemInfoSelectors: SystemInfoSelectors
+  ) {
     this.branchSubscription$ = this.branchSelectors.selectedBranch$;
     this.serviceSubscription$ = this.calendarServiceSelectors.selectedServices$;
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if(changes['editAppointment'] && this.editAppointment) {
+    if (changes["editAppointment"] && this.editAppointment) {
       this.enableReschedule = false;
-      window['moment'] = moment;
-      const calculatedAppointmentTime = moment(this.editAppointment.start).tz(this.editAppointment.branch.fullTimeZone);
-      if(calculatedAppointmentTime.isAfter(moment.now())) {
-        this.originalAppointmentTime = calculatedAppointmentTime.format('HH:mm');
-        this.selectedDates = [{selected: true, mDate: calculatedAppointmentTime}];
+      window["moment"] = moment;
+      const calculatedAppointmentTime = moment(this.editAppointment.start).tz(
+        this.editAppointment.branch.fullTimeZone
+      );
+      if (calculatedAppointmentTime.isAfter(moment.now())) {
+        this.originalAppointmentTime = calculatedAppointmentTime.format(
+          "HH:mm"
+        );
+        this.selectedDates = [
+          { selected: true, mDate: calculatedAppointmentTime }
+        ];
         this.isOriginalAppointmentTimeChanged = false;
       }
       this.fetchReservableDates();
-      
     }
   }
 
   ngOnInit() {
-    const branchSubscription = this.branchSubscription$.subscribe((cb) => {
+    const branchSubscription = this.branchSubscription$.subscribe(cb => {
       this.selectedBranch = cb;
     });
-    
 
-    const reservableDatesSub = this.reserveSelectors.reservableDates$.subscribe((dates: moment.Moment[]) => {
-      this.reservableDates = dates;
-      if(this.editAppointment){
-          if(this.reservableDates[0].isAfter(this.editAppointment.start)){
-            this.selectedDates =  [{
-              mDate: this.reservableDates[0],
-              selected: true
-            }];
-      }else{
-         this.selectedDates =  [{
-              mDate: moment(this.editAppointment.start),
-              selected: true
-            }];
+    const reservableDatesSub = this.reserveSelectors.reservableDates$.subscribe(
+      (dates: moment.Moment[]) => {
+        this.reservableDates = dates;
+        if (this.editAppointment) {
+          if (this.reservableDates[0].isAfter(this.editAppointment.start)) {
+            this.selectedDates = [
+              {
+                mDate: this.reservableDates[0],
+                selected: true
+              }
+            ];
+          } else {
+            this.selectedDates = [
+              {
+                mDate: moment(this.editAppointment.start),
+                selected: true
+              }
+            ];
+          }
+        }
       }
-      }
-    
-    
- 
-    });
+    );
 
-    const serviceSubscription = this.serviceSubscription$.subscribe((s) => {
+    const serviceSubscription = this.serviceSubscription$.subscribe(s => {
       this.selectedServices = s;
     });
 
     const uttSubscription = this.servicePointSelectors.uttParameters$
-    .subscribe(uttParameters => {
-      if (uttParameters) {
-        this.isRescheduleEnabledInUtt = uttParameters.reSheduleAppointment;
-        this.isDeleteEnabledInUtt = uttParameters.delAppointment;
+      .subscribe(uttParameters => {
+        if (uttParameters) {
+          this.isRescheduleEnabledInUtt = uttParameters.reSheduleAppointment;
+          this.isDeleteEnabledInUtt = uttParameters.delAppointment;
+        }
+      })
+      .unsubscribe();
+
+    const calendarBranchsSub = this.calendarBranchSelectors.branches$.subscribe(
+      bs => {
+        if (this.selectedBranch) {
+          this.selectedCalendarBranch = bs.find(
+            x => x.qpId == this.selectedBranch.id
+          );
+        }
       }
-    })
-    .unsubscribe();
+    );
 
-    const calendarBranchsSub = this.calendarBranchSelectors.branches$.subscribe((bs) => {
-      if(this.selectedBranch) {
-        this.selectedCalendarBranch = bs.find(x => x.qpId == this.selectedBranch.id);
+    const timeConventionSub = this.systemInfoSelectors.timeConvention$.subscribe(
+      tc => {
+        this.timeConvention = tc;
       }
-  }); 
+    );
 
-  this.subscriptions.add(calendarBranchsSub);
-
-   this.subscriptions.add(uttSubscription);
+    this.subscriptions.add(calendarBranchsSub);
+    this.subscriptions.add(timeConventionSub);
+    this.subscriptions.add(uttSubscription);
     this.subscriptions.add(branchSubscription);
     this.subscriptions.add(reservableDatesSub);
     this.subscriptions.add(serviceSubscription);
@@ -149,13 +194,17 @@ export class QmRescheduleComponent implements OnInit, OnDestroy {
   }
 
   onSelectDate(date: CalendarDate) {
-    if (this.editAppointment && this.editAppointment.services && this.editAppointment.services.length > 0) {
+    if (
+      this.editAppointment &&
+      this.editAppointment.services &&
+      this.editAppointment.services.length > 0
+    ) {
       this.currentlyActiveDate = date;
       this.timeSlotDispatchers.selectTimeslotDate(date.mDate);
       this.getTimeSlots();
       this.reservationExpiryTimerDispatchers.hideReservationExpiryTimer();
       this.timeSlotDispatchers.selectTimeslot(null);
-      if(moment(this.editAppointment.start).date() !== date.mDate.date()) {
+      if (moment(this.editAppointment.start).date() !== date.mDate.date()) {
         this.originalAppointmentTime = null;
       }
     }
@@ -163,7 +212,13 @@ export class QmRescheduleComponent implements OnInit, OnDestroy {
 
   onTimeSlotSelect(time: { title: string }) {
     this.rescheduleTime = time.title;
-    this.enableReschedule = moment(this.editAppointment.start).tz(this.selectedCalendarBranch.fullTimeZone).format('YYYY-MM-DD HH:mm') != (this.currentlyActiveDate.mDate.clone().format('YYYY-MM-DD') + ' ' + time.title);
+    this.enableReschedule =
+      moment(this.editAppointment.start)
+        .tz(this.selectedCalendarBranch.fullTimeZone)
+        .format("YYYY-MM-DD HH:mm") !=
+      this.currentlyActiveDate.mDate.clone().format("YYYY-MM-DD") +
+        " " +
+        time.title;
   }
 
   private getTimeSlots() {
@@ -171,7 +226,7 @@ export class QmRescheduleComponent implements OnInit, OnDestroy {
       branchPublicId: this.editAppointment.branch.publicId,
       serviceQuery: this.getServicesQueryString(),
       numberOfCustomers: this.noOfCustomers,
-      date: this.currentlyActiveDate.mDate.format('YYYY-MM-DD'),
+      date: this.currentlyActiveDate.mDate.format("YYYY-MM-DD"),
       time: this.editAppointment.startTime
     };
 
@@ -179,43 +234,114 @@ export class QmRescheduleComponent implements OnInit, OnDestroy {
   }
 
   getServicesQueryString(): string {
-    return this.editAppointment.services.reduce((queryString, service: ICalendarService) => {
-      return queryString + `;servicePublicId=${service.publicId}`;
-    }, '');
+    return this.editAppointment.services.reduce(
+      (queryString, service: ICalendarService) => {
+        return queryString + `;servicePublicId=${service.publicId}`;
+      },
+      ""
+    );
   }
 
   onDeleteAppointment() {
-    this.qmModalService.openForTransKeys('', 'confirm_delete', 'yes', 'no', (result) => {
-      if (result) {
-        this.appointmentDispatchers.deleteAppointment(this.editAppointment, () => {
-        },
-          (error) => {
-          }
-        );
+    this.qmModalService.openForTransKeys(
+      "",
+      "confirm_delete",
+      "yes",
+      "no",
+      result => {
+        if (result) {
+          this.appointmentDispatchers.deleteAppointment(
+            this.editAppointment,
+            () => {},
+            error => {}
+          );
+          this.onFlowExit.next(true);
+        }
+      },
+      () => {
         this.onFlowExit.next(true);
       }
-    }, () => {
-      this.onFlowExit.next(true);
-    });
+    );
   }
 
   onRescheduleAppointment() {
     if (this.enableReschedule) {
-      this.qmModalService.openForTransKeys('', 'confirm_reschedule', 'yes', 'no', (result) => {
-        if (result) {
-          let rescheduleAppointment = this.editAppointment;
-          const originalAppointmentStartTime = this.editAppointment.start;
-          rescheduleAppointment.start = `${this.currentlyActiveDate.mDate.format('YYYY-MM-DD')}T${this.rescheduleTime}`;
+      this.qmModalService.openForTransKeys(
+        "",
+        "confirm_reschedule",
+        "yes",
+        "no",
+        result => {
+          if (result) {
+            let rescheduleAppointment = this.editAppointment;
+            const originalAppointmentStartTime = this.editAppointment.start;
+            rescheduleAppointment.start = `${this.currentlyActiveDate.mDate.format(
+              "YYYY-MM-DD"
+            )}T${this.rescheduleTime}`;
 
-          const timeDiff = moment.duration(moment(this.editAppointment.end).diff(originalAppointmentStartTime));
-          let endTime = moment(rescheduleAppointment.start).add(timeDiff.asMinutes(), 'minutes');
-          rescheduleAppointment.end = `${endTime.format('YYYY-MM-DD')}T${endTime.format('HH:mm')}`;
-          this.appointmentDispatchers.rescheduleAppointment(rescheduleAppointment);
-          this.onFlowExit.next(true);
-        }
-      }, () => {
-
-      });
+            const timeDiff = moment.duration(
+              moment(this.editAppointment.end).diff(
+                originalAppointmentStartTime
+              )
+            );
+            let endTime = moment(rescheduleAppointment.start).add(
+              timeDiff.asMinutes(),
+              "minutes"
+            );
+            rescheduleAppointment.end = `${endTime.format(
+              "YYYY-MM-DD"
+            )}T${endTime.format("HH:mm")}`;
+            this.appointmentDispatchers.rescheduleAppointment(
+              rescheduleAppointment
+            );
+            this.onFlowExit.next(true);
+          }
+        },
+        () => {}
+      );
     }
+  }
+
+  getSelectedAppointmentInfoTime() {
+    let appointmentInfo = "";
+    let timeformat = "hh:mm A";
+    if (this.timeConvention === "24") {
+      timeformat = "HH:mm";
+    }
+
+    if (this.editAppointment) {
+      appointmentInfo += moment(this.editAppointment.start)
+        .tz(
+          this.editAppointment.branch.fullTimeZone ||
+            this.selectedCalendarBranch.fullTimeZone
+        )
+        .format(timeformat);
+    }
+
+    return appointmentInfo;
+  }
+
+  getSelectedAppointmentInfoDate() {
+    let appointmentInfo = "";
+
+    if (this.editAppointment) {
+      //time data
+        appointmentInfo += moment(this.editAppointment.start)
+          .tz(
+            this.editAppointment.branch.fullTimeZone ||
+              this.selectedCalendarBranch.fullTimeZone
+          )
+          .format("DD/MM/YYYY");
+    }
+    return appointmentInfo;
+  }
+
+  getSelectedAppointmentInfoCustomer() {
+    let appointmentInfo = "";
+    if (this.editAppointment && this.editAppointment.customers[0]) {
+      appointmentInfo += `${this.editAppointment.customers[0].firstName} `;
+      appointmentInfo += `${this.editAppointment.customers[0].lastName}`;
+    }
+    return appointmentInfo;
   }
 }

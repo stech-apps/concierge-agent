a83a30a0b25c50ccf0d764d9676363a8765a0588
udara <Udara.SampathBandara@qmatic.com>
2019-07-05 10:47:18 +0530

    166948016: x model tabbing issue fixed

--- src/app/components/presentational/qm-profile/qm-profile.component.ts
+++ src/app/components/presentational/qm-profile/qm-profile.component.ts
@@ -177,6 +177,7 @@ export class QmProfileComponent implements OnInit, OnDestroy, AfterViewInit {
   ngOnInit() {
     this.queueService.stopQueuePoll();
     setTimeout(() => {
+      document.getElementById('qm-profile-page-title').focus();
       if( this.isEnableUseDefault && ((this.selectedBranch.id == -1) &&
         (this.selectedServicePoint.id==-1))  ){
           this.isEnableUseDefault =false;

decccf6a46fbaf41a43d482855d0e103ba95b438
udara <Udara.SampathBandara@qmatic.com>
2019-08-01 10:13:46 +0530

    167084539: Accessible date picker adjustments

--- src/app/components/containers/qm-calendar/qm-calendar.component.ts
+++ src/app/components/containers/qm-calendar/qm-calendar.component.ts
@@ -5,6 +5,8 @@ import * as moment from 'moment';
 import * as _ from 'underscore';
 import { Observable } from 'rxjs';
 import { UserSelectors } from 'src/store';
+import { ToastService } from 'src/util/services/toast.service';
+import { TranslateService } from '@ngx-translate/core';
 
 export interface CalendarDate {
   mDate: moment.Moment;
@@ -21,6 +23,7 @@ export interface CalendarDate {
 export class QmCalendarComponent implements OnInit, OnChanges {
 
   currentDate = moment();
+  TempDate = moment();
   dayNames: string[];
   // dayNames = ['calendar.weekday.sun', 'calendar.weekday.mon', 'calendar.weekday.tue', 'calendar.weekday.wed', 'calendar.weekday.thu', 'calendar.weekday.fri', 'calendar.weekday.sat'];
   weeks: CalendarDate[][] = [];
@@ -40,11 +43,14 @@ export class QmCalendarComponent implements OnInit, OnChanges {
 
   private _currentCalendarDates: CalendarDate[] = [];
 
-  constructor(private userSelectors: UserSelectors) {
+  constructor(private userSelectors: UserSelectors,
+    private toastService: ToastService,
+    private translate: TranslateService) {
     this.userDirection$ = this.userSelectors.userDirection$;
   }
 
   ngOnInit(): void {
+    // set locale
     this.userSelectors.userLocale$.subscribe(
       locale => {
         if (locale) {
@@ -55,22 +61,21 @@ export class QmCalendarComponent implements OnInit, OnChanges {
     );
     this.generateCalendar();
   }
-
-  focusFirstDate(){
+  // Focus the first element of the calander date when focus the calendar container
+  focusFirstDate() {
     var focusable = document.getElementById("qm-calendar").querySelectorAll('button:not([disabled])');
     setTimeout(() => {
       if (focusable.length > 0) {
-        focusable[0].setAttribute("name","firstElement");
+        focusable[0].setAttribute("name", "firstElement");
         setTimeout(() => {
           document.getElementsByName("firstElement")[0].focus();
         }, 500);
-      }          
+      }
     }, 10);
   }
+  // Go to timeslot selector when press the tab in the calandar container
   TabPressed() {
     document.getElementById('qm-timeslot-container').focus();
-    // console.log("blur");
-    
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -78,7 +83,6 @@ export class QmCalendarComponent implements OnInit, OnChanges {
       changes.selectedDates.currentValue &&
       changes.selectedDates.currentValue.length > 1) || (changes.enabledDates && changes.enabledDates.currentValue && changes.enabledDates.currentValue.length > 0)) {
       // sort on date changes for better performance when range checking
-
       if (changes.selectedDates) {
         this.sortedDates = _.sortBy(changes.selectedDates.currentValue, (m: CalendarDate) => m.mDate.valueOf());
       }
@@ -90,38 +94,291 @@ export class QmCalendarComponent implements OnInit, OnChanges {
     this.generateCalendar();
   }
 
-  // arrow functions
-  KeyarrowUp(value : string) {
-    if(document.getElementById(parseInt(value) - 7 + '-enabled') ){
-      document.getElementById(parseInt(value) - 7 + '-enabled').focus();
+  // arrow functions //
+
+  // When up arrow pressed
+  KeyarrowUp(year: string, month: string, day: string) {
+    // If date is available in the same month select the date
+    if (document.getElementById(month + '-' + (parseInt(day) - 7) + '-enabled')) {
+      document.getElementById(month + '-' + (parseInt(day) - 7) + '-enabled').focus();
+    } else {
+      var isFocused = false;
+      // get the previous week date
+      this.TempDate = moment(year + '-' + (parseInt(month) + 1).toString() + '-' + day, "YYYY-MM-DD").subtract(1, 'week');
+      // check date is a enabled date
+      this.enabledDates.forEach(ed => {
+        if (ed.isSame(this.TempDate)) {
+          if ((parseInt(month)).toString() != this.TempDate.month().toString()) {
+            this.prevMonth();
+            setTimeout(() => {
+              if (document.getElementById(`${this.TempDate.month()}-${this.TempDate.date()}-enabled`)) {
+                document.getElementById(`${this.TempDate.month()}-${this.TempDate.date()}-enabled`).focus();
+                isFocused = true;
+              }
+            }, 100);
+          }
+        }
+      });
+      // if could not focus show a error message 
+      setTimeout(() => {
+        if (isFocused == false) {
+          const translateSubscription = this.translate
+            .get("no_enabled_date")
+            .subscribe((res: string) => {
+              this.toastService.errorToast(res);
+            });
+          translateSubscription.unsubscribe();
+        }
+      }, 200);
+
     }
-    
+
   }
-  KeyarrowDown(value : string) {
-    if(document.getElementById(parseInt(value) + 7 + '-enabled') ){
-      document.getElementById(parseInt(value) + 7 + '-enabled').focus();
+  // When down arrow is pressed
+  KeyarrowDown(year: string, month: string, day: string) {
+    // If date is available in the same month
+    if (document.getElementById(month + '-' + (parseInt(day) + 7) + '-enabled')) {
+      document.getElementById(month + '-' + (parseInt(day) + 7) + '-enabled').focus();
+    } else {
+      var isFocused = false;
+      // get the next week date
+      this.TempDate = moment(year + '-' + (parseInt(month) + 1).toString() + '-' + day, "YYYY-MM-DD").add(1, 'week');
+      // check date is a enabled date
+      this.enabledDates.forEach(ed => {
+        if (ed.isSame(this.TempDate)) {
+          if ((parseInt(month)).toString() != this.TempDate.month().toString()) {
+            this.nextMonth();
+            setTimeout(() => {
+              if (document.getElementById(`${this.TempDate.month()}-${this.TempDate.date()}-enabled`)) {
+                document.getElementById(`${this.TempDate.month()}-${this.TempDate.date()}-enabled`).focus();
+                isFocused = true;
+              }
+            }, 100);
+          }
+        }
+      });
+      // if could not focus show a error message 
+      setTimeout(() => {
+        if (isFocused == false) {
+          const translateSubscription = this.translate
+            .get("no_enabled_date")
+            .subscribe((res: string) => {
+              this.toastService.errorToast(res);
+            });
+          translateSubscription.unsubscribe();
+        }
+      }, 200);
     }
-    
+
   }
-  KeyarrowLeft(value : string) {
-    if(document.getElementById(parseInt(value) - 1 + '-enabled') ){
-      document.getElementById(parseInt(value) - 1 + '-enabled').focus();
+  // When left arrow pressed
+  KeyarrowLeft(month: string, day: string) {
+    var prevDayDone = false;
+    for (var i = parseInt(day); i >= 1; i--) {
+      if (document.getElementById(month + '-' + (i - 1) + '-enabled')) {
+        document.getElementById(month + '-' + (i - 1) + '-enabled').focus();
+        prevDayDone = true;
+        break;
+      }
     }
-    
-  }
-  KeyarrowRight(value : string) {
-    if(document.getElementById(parseInt(value) + 1 + '-enabled') ){
-      document.getElementById(parseInt(value) + 1 + '-enabled').focus();
+    // if there is no date in current month go to previous month to check the date
+    if (prevDayDone == false) {
+      this.prevMonth();
+      setTimeout(() => {
+        for (var i = 31; i >= 1; i--) {
+          // if current month is january go to december else substract one from current month
+          if (month == '0') {
+            if (document.getElementById('11-' + (i - 1) + '-enabled')) {
+              document.getElementById('11-' + (i - 1) + '-enabled').focus();
+              prevDayDone = true;
+              break;
+            }
+          } else {
+            if (document.getElementById((parseInt(month) - 1) + '-' + (i - 1) + '-enabled')) {
+              document.getElementById((parseInt(month) - 1) + '-' + (i - 1) + '-enabled').focus();
+              prevDayDone = true;
+              break;
+            }
+          }
+        }
+      }, 100);
+      // if could not focus show a error message after going to previous month
+      setTimeout(() => {
+        if (prevDayDone == false) {
+          const translateSubscription = this.translate
+            .get("no_enabled_date")
+            .subscribe((res: string) => {
+              this.toastService.errorToast(res);
+            });
+          translateSubscription.unsubscribe();
+          this.nextMonth();
+        }
+      }, 200);
     }
-    
   }
-  onKeydown(event) {
-    if(event.shiftKey && event.keyCode == 9) {
-        document.getElementById("qm-next-month").focus();
-        event.stopPropagation();
-        event.preventDefault();        
-    }    
+  // When right arrow key is pressed go to the next date
+  KeyarrowRight(month: string, day: string) {
+    var nextDayDone = false;
+    for (var i = parseInt(day); i <= 31; i++) {
+      if (document.getElementById(month + '-' + (i + 1) + '-enabled')) {
+        document.getElementById(month + '-' + (i + 1) + '-enabled').focus();
+        nextDayDone = true;
+        break;
+      }
+    }
+    // if couldn't find the next date on the current month go to the next month for date search
+    if (nextDayDone == false) {
+      this.nextMonth();
+      // if current month is december go to january else add one to the current month number
+      if (month == '11') {
+        for (var i = 0; i <= 31; i++) {
+          if (document.getElementById('0-' + (i + 1) + '-enabled')) {
+            document.getElementById('0-' + (i + 1) + '-enabled').focus();
+            nextDayDone = true;
+            break;
+          }
+        }
+      } else {
+        setTimeout(() => {
+          for (var i = 0; i <= 31; i++) {
+            if (document.getElementById((parseInt(month) + 1) + '-' + (i + 1) + '-enabled')) {
+              document.getElementById((parseInt(month) + 1) + '-' + (i + 1) + '-enabled').focus();
+              nextDayDone = true;
+              break;
+            }
+          }
+        }, 100);
+        // if could not focus show a error message 
+        setTimeout(() => {
+          if (nextDayDone == false) {
+            const translateSubscription = this.translate
+              .get("no_enabled_date")
+              .subscribe((res: string) => {
+                this.toastService.errorToast(res);
+              });
+            translateSubscription.unsubscribe();
+            this.prevMonth();
+          }
+        }, 200);
+      }
+    }
   }
+  // When press any key 
+  onKeydown(event, month: string, date: string, firstDay: string, lastDay: string) {
+    // when user tab back (alt + tab) go to previous selector
+    if (event.shiftKey && event.keyCode == 9) {
+      document.getElementById("qm-next-month").focus();
+      event.stopPropagation();
+      event.preventDefault();
+    }
+    // alt + page up to next year
+    else if (event.altKey && event.keyCode == 33) {
+      this.nextYear();
+      setTimeout(() => {
+        if (document.getElementById(month + '-' + date + '-enabled')) {
+          document.getElementById(month + '-' + date + '-enabled').focus();
+        } else {
+          const translateSubscription = this.translate
+            .get("no_enabled_date")
+            .subscribe((res: string) => {
+              this.toastService.errorToast(res);
+            });
+          translateSubscription.unsubscribe();
+          this.prevYear();
+        }
+      }, 100);
+    }
+    // alt + page down to previous year
+    else if (event.altKey && event.keyCode == 34) {
+      this.prevYear();
+      setTimeout(() => {
+        if (document.getElementById(month + '-' + date + '-enabled')) {
+          document.getElementById(month + '-' + date + '-enabled').focus();
+        } else {
+          const translateSubscription = this.translate
+            .get("no_enabled_date")
+            .subscribe((res: string) => {
+              this.toastService.errorToast(res);
+            });
+          translateSubscription.unsubscribe();
+          this.nextYear();
+        }
+      }, 100);
+    }
+    // page up to next month
+    else if (event.keyCode == 33) {
+      this.nextMonth();
+      if(month == '11') {
+        month = '0';
+      } else {
+        month = (parseInt(month)+1).toString();
+      }
+      setTimeout(() => {
+        if (document.getElementById(month + '-' + date + '-enabled')) {
+          document.getElementById(month + '-' + date + '-enabled').focus();
+        } else {
+          const translateSubscription = this.translate
+            .get("no_enabled_date")
+            .subscribe((res: string) => {
+              this.toastService.errorToast(res);
+            });
+          translateSubscription.unsubscribe();
+          this.prevMonth();
+        }
+      }, 100);
+    }
+    // page down to previous month
+    else if (event.keyCode == 34) {
+      this.prevMonth();
+      if(month == '0') {
+        month = '11';
+      } else {
+        month = (parseInt(month)-1).toString();
+      }
+      setTimeout(() => {
+        if (document.getElementById(month + '-' + date + '-enabled')) {
+          document.getElementById(month + '-' + date + '-enabled').focus();
+        } else {
+          const translateSubscription = this.translate
+            .get("no_enabled_date")
+            .subscribe((res: string) => {
+              this.toastService.errorToast(res);
+            });
+          translateSubscription.unsubscribe();
+          this.nextMonth();
+        }
+      }, 100);
+    }
+    // Home button to first day of the week 
+    else if (event.keyCode == 36) {
+      // if first day of the week in prev month set last date to 1
+      if (parseInt(firstDay) > parseInt(date)) {
+        firstDay = '1';
+      }
+      // first Day of the week
+      for (var i = parseInt(firstDay); i < parseInt(date); i++) {
+        if (document.getElementById(month + '-' + (i) + '-enabled')) {
+          document.getElementById(month + '-' + (i) + '-enabled').focus();
+          break;
+        }
+      }
+    }
+    // End button to last day of the week
+    else if (event.keyCode == 35) {
+      // if last day of the week in next month set last date to 31
+      if (parseInt(lastDay) < parseInt(date)) {
+        lastDay = '31';
+      }
+      // last of the week
+      for (var i = parseInt(lastDay); i > parseInt(date); i--) {
+        if (document.getElementById(month + '-' + (i) + '-enabled')) {
+          document.getElementById(month + '-' + (i) + '-enabled').focus();
+          break;
+        }
+      }
+    }
+  }
+
 
   // date checkers
 
@@ -185,6 +442,25 @@ export class QmCalendarComponent implements OnInit, OnChanges {
 
   nextYear(): void {
     this.currentDate = moment(this.currentDate).add(1, 'year');
+    this.generateCalendar();
+  }
+
+  // Accessibility keyboard function support moment functions
+  calendarNextWeek(): void {
+    this.currentDate = moment(this.currentDate).add(1, 'week');
+    this.generateCalendar();
+
+  }
+  calendarPrevWeek() {
+    this.currentDate = moment(this.currentDate).subtract(1, 'week');
+    this.generateCalendar();
+  }
+  calendarNextDay(): void {
+    this.currentDate = moment(this.currentDate).add(1, 'day');
+    this.generateCalendar();
+  }
+  calendarPrevDay(): void {
+    this.currentDate = moment(this.currentDate).subtract(1, 'day');
     this.generateCalendar();
   }
 
@@ -261,5 +537,7 @@ export class QmCalendarComponent implements OnInit, OnChanges {
         return calDay;
       });
   }
-
+  getDateName(mDate) {
+    return moment(mDate.mDate._d).format('MMMM Do YYYY');
+  }
 }
